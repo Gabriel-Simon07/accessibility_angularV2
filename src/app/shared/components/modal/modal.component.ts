28e5d98e2e350847884412cd11a8dc7514cba812
Gabriel Simon <gabrielsimon775@gmail.com>
2022-07-12 21:18:02 -0300

    Modal component being is implemeting

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { ModalConfig } from "./interfaces/modal-config";
+
+@Component({
+ selector: 'app-modal',
+ templateUrl: './modal.component.html',
+ styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+
+  public config: ModalConfig;
+}
---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, TemplateRef } from "@angular/core";
+import { ModalConfig } from "../interfaces/modal-config";
+
+@Injectable()
+export class ModalService {
+
+  public open(modalConfig: ModalConfig): ModalRef {
+    console.log('open called');
+    return new ModalRef();
+  }
+}
+
+export class ModalRef {
+
+  public close(): void {
+    console.log('close called');
+  }
+}
