27dbebce47f328b3eae8272ce1a1c07f844663aa
Gabriel Simon <gabrielsimon775@gmail.com>
2022-07-14 20:20:13 -0300

    TemplateRef examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, TemplateRef, ViewChild } from '@angular/core';
 import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
@@ -8,13 +8,22 @@ import { ModalRef, ModalService } from './shared/components/modal/services/modal
 })
 export class AppComponent {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+  @ViewChild('template1') public template1: TemplateRef<any>;
+  @ViewChild('template2') public template2: TemplateRef<any>;
+  public selectedTemplate: TemplateRef<any>;
+
   title = 'a11y-p2';
   public firstName = 'Gabriel';
   public modalRef: ModalRef;
 
   constructor(private modalService: ModalService) {}
 
+  public ngAfterViewInit(): void {
+    this.selectedTemplate = this.template1;
+  }
+
   public show() {
+    this.selectedTemplate = this.template2;
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details'
